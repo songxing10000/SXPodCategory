8576113cc45106b698dd66e73d049c9db7a2d6d9
songxing <songxing@che001.com>
2018-03-16 14:45:23 +0800

    异常情况处理

--- SXPodCategory/Classes/UI/UIView+SXPodCategory_GetController.m
+++ SXPodCategory/Classes/UI/UIView+SXPodCategory_GetController.m
@@ -17,9 +17,49 @@
         return (UIViewController *)self.nextResponder;
     }
     if (self.superview == nil) {
-        NSAssert(NO, @"此控件没有父控件");
+        return [self topViewController];
     }
     return self.superview.getViewController;
 }
-
+//获取当前屏幕显示的viewcontroller
+- (UIViewController*)topViewController {
+    return [self topViewControllerWithRootViewController:[self windowRootViewController]];
+}
+- (UIViewController *)windowRootViewController{
+    UIViewController *result = nil;
+    UIWindow * window = [[UIApplication sharedApplication] keyWindow];
+    if (window.windowLevel != UIWindowLevelNormal){
+        NSArray *windows = [[UIApplication sharedApplication] windows];
+        for(UIWindow * tmpWin in windows){
+            if (tmpWin.windowLevel == UIWindowLevelNormal){
+                window = tmpWin;
+                break;
+            }
+        }
+    }
+    
+    UIView *frontView = [[window subviews] objectAtIndex:0];
+    id nextResponder = [frontView nextResponder];
+    
+    if ([nextResponder isKindOfClass:[UIViewController class]])
+        result = nextResponder;
+    else
+        result = window.rootViewController;
+    
+    return result;
+}
+- (UIViewController*)topViewControllerWithRootViewController:(UIViewController*)rootViewController {
+    if ([rootViewController isKindOfClass:[UITabBarController class]]) {
+        UITabBarController* tabBarController = (UITabBarController*)rootViewController;
+        return [self topViewControllerWithRootViewController:tabBarController.selectedViewController];
+    } else if ([rootViewController isKindOfClass:[UINavigationController class]]) {
+        UINavigationController* navigationController = (UINavigationController*)rootViewController;
+        return [self topViewControllerWithRootViewController:navigationController.visibleViewController];
+    } else if (rootViewController.presentedViewController) {
+        UIViewController* presentedViewController = rootViewController.presentedViewController;
+        return [self topViewControllerWithRootViewController:presentedViewController];
+    } else {
+        return rootViewController;
+    }
+}
 @end
