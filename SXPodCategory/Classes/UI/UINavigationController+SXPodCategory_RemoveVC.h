a36505bc7cc2e8c6e152c4ffd36114dad4241065
songxing <songxing@che001.com>
2018-07-30 15:25:01 +0800

    增加 方法 - (void)backToController:(NSString *)ctrlClassName animated:(BOOL)animate;

--- SXPodCategory/Classes/UI/UINavigationController+SXPodCategory_RemoveVC.h
+++ SXPodCategory/Classes/UI/UINavigationController+SXPodCategory_RemoveVC.h
@@ -14,4 +14,13 @@
  移除堆栈里类型为 VCClass 的控制器
  */
 - (void)removeVCClass:(Class)aVCClass;
+
+/**
+ 调回到某个页面
+
+ @param ctrlClassName 页面的类名字符串
+ @param animate 是否动画
+ */
+- (void)backToController:(NSString *)ctrlClassName animated:(BOOL)animate;
+
 @end
--- SXPodCategory/Classes/UI/UINavigationController+SXPodCategory_RemoveVC.m
+++ SXPodCategory/Classes/UI/UINavigationController+SXPodCategory_RemoveVC.m
@@ -11,9 +11,8 @@
 @implementation UINavigationController (SXPodCategory_RemoveVC)
 
 - (void)removeVCClass:(Class)aVCClass{
-    
     NSMutableArray <UIViewController *> *vcs = self.viewControllers.mutableCopy;
-    
+
     [vcs enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(UIViewController * _Nonnull vc, NSUInteger idx, BOOL * _Nonnull stop) {
         if ([vc isKindOfClass:[aVCClass class]]) {
             [vc.view removeFromSuperview];
@@ -22,4 +21,14 @@
     }];
     self.viewControllers = vcs;
 }
+- (void)backToController:(NSString *)ctrlClassName animated:(BOOL)animate {
+    NSArray<UIViewController *> *controllers = self.viewControllers;
+    NSArray<UIViewController *> *result = [controllers filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(id  _Nullable evaluatedObject, NSDictionary<NSString *,id> * _Nullable bindings) {
+        return [evaluatedObject isKindOfClass:NSClassFromString(ctrlClassName)];
+    }]];
+    if (result.count > 0) {
+        [self popToViewController:result.firstObject animated:YES];
+    }
+}
+
 @end
