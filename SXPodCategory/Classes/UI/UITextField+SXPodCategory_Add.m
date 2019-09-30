7e7831a9572e0f6930426009a56560de42e388c8
songxing <songxing@kaiyuan.net>
2019-09-30 11:05:15 +0800

    加入了一些注释，适配了iOS13的_placeholderLabel.textColor换为placeholderLabel.textColor访问

--- SXPodCategory/Classes/UI/UITextField+SXPodCategory_Add.m
+++ SXPodCategory/Classes/UI/UITextField+SXPodCategory_Add.m
@@ -7,8 +7,24 @@
 //
 
 #import "UITextField+SXPodCategory_Add.h"
-
-static NSString *const kUITextFieldPlaceholderColorKey = @"_placeholderLabel.textColor";
+/*
+ 
+ // 崩溃 api。获取 _placeholderLabel 不会崩溃，但是获取 _placeholderLabel 里的属性就会
+ [textField setValue:[UIColor blueColor] forKeyPath:@"_placeholderLabel.textColor"];
+ [textField setValue:[UIFont systemFontOfSize:20] forKeyPath:@"_placeholderLabel.font"];
+ 
+ // 替代方案 1，去掉下划线，访问 placeholderLabel
+ [textField setValue:[UIColor blueColor] forKeyPath:@"placeholderLabel.textColor"];
+ [textField setValue:[UIFont systemFontOfSize:20] forKeyPath:@"placeholderLabel.font"];
+ 
+ // 替代方案 2
+ textField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"输入" attributes:@{
+ NSForegroundColorAttributeName: [UIColor blueColor],
+ NSFontAttributeName: [UIFont systemFontOfSize:20]
+ }];
+ 
+ */
+static NSString *const kUITextFieldPlaceholderColorKey = @"placeholderLabel.textColor";
 
 @implementation UITextField (SXPodCategory_Add)
 
