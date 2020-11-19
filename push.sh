1ca091aa28d8e5eaba74c1fde184a986a8a8bd47
songxing10000 <songxing10000@live.cn>
2020-11-19 17:22:51 +0800

    up

--- push.sh
+++ push.sh
@@ -4,3 +4,6 @@ pod trunk push SXPodCategory.podspec  --allow-warnings
 
 # 私有库
 # pod repo push ckkspecs WeexSDK.podspec --allow-warnings
+
+# 注册
+# pod trunk register songxing10000@live.cn songxing1000
